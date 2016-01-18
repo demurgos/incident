6faf218330a83f8e62f898fcb46f469c26739471
Demurgos <demurgos.net@gmail.com>
2016-01-18 23:05:28 +0100

    Simplified constructor, build with definitions

---
+++ src/core/Incident.spec.ts
@@ -0,0 +1,9 @@
+import {Incident} from './Incident';
+import * as chai from 'chai';
+
+describe("Incident", function() {
+    it("should inherit from the native Error", function () {
+        var foo = new Incident();
+        chai.assert.instanceOf(foo, Error);
+    });
+});
---
+++ src/core/utils.ts
@@ -0,0 +1,42 @@
+interface ErrorConstructor {
+    new (message?: string): Error;
+    (message?: string): Error;
+    prototype: Error
+    captureStackTrace:(targetObject:any, constructorOpt?:any)=>void;
+}
+
+interface Error {
+    stack: string;
+}
+
+declare var Error:ErrorConstructor;
+
+export let captureStackTrace:(targetObject:any, constructorOpt?:any)=>void;
+if (Error.captureStackTrace) {
+    captureStackTrace = Error.captureStackTrace;
+} else {
+    captureStackTrace = function captureStackTrace (error, ctr) {
+        let container = new Error();
+
+        Object.defineProperty(error, 'stack', {
+            configurable: true,
+            get: function getStack () {
+                let stack = container.stack;
+
+                // Replace property with value for faster future accesses.
+                Object.defineProperty(this, 'stack', {
+                    value: stack
+                });
+
+                return stack
+            },
+            set: function setStack (stack) {
+                Object.defineProperty(error, 'stack', {
+                    configurable: true,
+                    value: stack,
+                    writable: true
+                });
+            }
+        });
+    }
+}
