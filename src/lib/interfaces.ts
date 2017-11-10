bdb65997efaa4fb798444caf86f8e3ea1244cc9b
Charles Samborski <demurgos@demurgos.net>
2017-11-10 15:41:01 +0100

    Fix support strict function variance checks
    Errors were caused by the type definition of the formatter. It
    was typed as `(data?: D) => string` even if data is always defined.
    This means that it wasn't possible to use functions with the
    `(data: D) => string` signature.

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -16,29 +16,29 @@ export interface Incident<Name extends string, Data extends {}, Cause extends (E
 
 /* tslint:disable:comment-format max-line-length typedef-whitespace */
 export interface StaticIncident extends Function {
-//new<                                C extends Error>(cause: C                                                              ): Incident<"Incident", {}, C        >;
-  new<                                C extends Error>(cause: C,                   message : string | ((data?: {}) => string)): Incident<"Incident", {}, C        >;
-//new<N extends string,               C extends Error>(cause: C, name: N,                                                    ): Incident<N,          {}, C        >;
-  new<N extends string,               C extends Error>(cause: C, name: N,          message : string | ((data?: {}) => string)): Incident<N,          {}, C        >;
-  new                                                 (                            message?: string | ((data?: {}) => string)): Incident<"Incident", {}, undefined>;
-//new<N extends string                               >(          name: N,                                                    ): Incident<N,          {}, undefined>;
-  new<N extends string                               >(          name: N,          message : string | ((data?: {}) => string)): Incident<N,          {}, undefined>;
-  new<N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data?: D) => string) ): Incident<N,          D,  C        >;
-  new<                  D extends {}, C extends Error>(cause: C,          data: D, message?: string | ((data?: D) => string) ): Incident<"Incident", D,  C        >;
-  new<N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data?: {}) => string)): Incident<N,          D,  undefined>;
-  new<                  D extends {}                 >(                   data: D, message?: string | ((data?: D ) => string)): Incident<"Incident", D,  undefined>;
+//new<                                C extends Error>(cause: C                                                             ): Incident<"Incident", {}, C        >;
+  new<                                C extends Error>(cause: C,                   message : string | ((data: {}) => string)): Incident<"Incident", {}, C        >;
+//new<N extends string,               C extends Error>(cause: C, name: N,                                                   ): Incident<N,          {}, C        >;
+  new<N extends string,               C extends Error>(cause: C, name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, C        >;
+  new                                                 (                            message?: string | ((data: {}) => string)): Incident<"Incident", {}, undefined>;
+//new<N extends string                               >(          name: N,                                                   ): Incident<N,          {}, undefined>;
+  new<N extends string                               >(          name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, undefined>;
+  new<N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  C        >;
+  new<                  D extends {}, C extends Error>(cause: C,          data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  C        >;
+  new<N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  undefined>;
+  new<                  D extends {}                 >(                   data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  undefined>;
 
-     <N extends string, D extends {}, C extends Error | undefined>(error: Error & {name: N, data?: D, cause?: C}             ): Incident<N,          D,  C        >;
-                                                                  (error: Error                                              ): Incident<string,     {}, undefined>;
+     <N extends string, D extends {}, C extends Error | undefined>(error: Error & {name: N, data: D, cause?: C}             ): Incident<N,          D,  C        >;
+                                                                  (error: Error                                             ): Incident<string,     {}, undefined>;
 
-     <                                C extends Error>(cause: C,                   message : string | ((data?: {}) => string)): Incident<"Incident", {}, C        >;
-//   <N extends string,               C extends Error>(cause: C, name: N,                                                    ): Incident<N,          {}, C        >;
-     <N extends string,               C extends Error>(cause: C, name: N,          message : string | ((data?: {}) => string)): Incident<N,          {}, C        >;
-                                                      (                            message?: string | ((data?: {}) => string)): Incident<"Incident", {}, undefined>;
-//   <N extends string                               >(          name: N,                                                    ): Incident<N,          {}, undefined>;
-     <N extends string                               >(          name: N,          message : string | ((data?: {}) => string)): Incident<N,          {}, undefined>;
-     <N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data?: D) => string) ): Incident<N,          D,  C        >;
-     <                  D extends {}, C extends Error>(cause: C,          data: D, message?: string | ((data?: D) => string) ): Incident<"Incident", D,  C        >;
-     <N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data?: {}) => string)): Incident<N,          D,  undefined>;
-     <                  D extends {}                 >(                   data: D, message?: string | ((data?: D ) => string)): Incident<"Incident", D,  undefined>;
+     <                                C extends Error>(cause: C,                   message : string | ((data: {}) => string)): Incident<"Incident", {}, C        >;
+//   <N extends string,               C extends Error>(cause: C, name: N,                                                   ): Incident<N,          {}, C        >;
+     <N extends string,               C extends Error>(cause: C, name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, C        >;
+                                                      (                            message?: string | ((data: {}) => string)): Incident<"Incident", {}, undefined>;
+//   <N extends string                               >(          name: N,                                                   ): Incident<N,          {}, undefined>;
+     <N extends string                               >(          name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, undefined>;
+     <N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  C        >;
+     <                  D extends {}, C extends Error>(cause: C,          data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  C        >;
+     <N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  undefined>;
+     <                  D extends {}                 >(                   data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  undefined>;
 }
