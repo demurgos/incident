3e6400f547565453c3554e8d0559312bb73cf78d
Charles Samborski <demurgos@demurgos.net>
2017-11-11 12:10:31 +0100

    Update types to require `name` parameter

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
-export interface Attributes<Name extends string, Data extends {}, Cause extends (Error | undefined)> {
+export interface Plain<Name extends string, Data extends {}, Cause extends (Error | undefined)> {
   message: string;
   name: Name;
   data: Data;
@@ -7,38 +7,26 @@ export interface Attributes<Name extends string, Data extends {}, Cause extends 
 }
 
 export interface Incident<Name extends string, Data extends {}, Cause extends (Error | undefined)>
-  extends Error, Attributes<Name, Data, Cause> {
+  extends Error, Plain<Name, Data, Cause> {
   name: Name;
   stack: string;
 
   toString(): string;
 }
 
-/* tslint:disable:comment-format max-line-length typedef-whitespace space-within-parens */
-export interface StaticIncident extends Function {
-//new<                                C extends Error>(cause: C                                                             ): Incident<"Incident", {}, C        >;
-  new<                                C extends Error>(cause: C,                   message : string | ((data: {}) => string)): Incident<"Incident", {}, C        >;
-//new<N extends string,               C extends Error>(cause: C, name: N,                                                   ): Incident<N,          {}, C        >;
-  new<N extends string,               C extends Error>(cause: C, name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, C        >;
-  new                                                 (                            message?: string | ((data: {}) => string)): Incident<"Incident", {}, undefined>;
-//new<N extends string                               >(          name: N,                                                   ): Incident<N,          {}, undefined>;
-  new<N extends string                               >(          name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, undefined>;
-  new<N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  C        >;
-  new<                  D extends {}, C extends Error>(cause: C,          data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  C        >;
-  new<N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  undefined>;
-  new<                  D extends {}                 >(                   data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  undefined>;
+// tslint:disable:comment-format max-line-length typedef-whitespace space-within-parens
+export interface StaticIncident {
+  new<N extends string,               C extends Error>(cause: C, name: N,          message?: string | ((data: {}) => string)): Incident<N,      {}, C        >;
+  new<N extends string                               >(          name: N,          message?: string | ((data: {}) => string)): Incident<N,      {}, undefined>;
+  new<N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,      D,  C        >;
+  new<N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,      D,  undefined>;
 
-     <N extends string, D extends {}, C extends Error | undefined>(error: Error & {name: N; data: D; cause?: C}             ): Incident<N,          D,  C        >;
-                                                                  (error: Error                                             ): Incident<string,     {}, undefined>;
+     <N extends string, D extends {}, C extends Error | undefined>(error: Error & {name: N; data: D; cause?: C}             ): Incident<N,      D,  C        >;
+                                                                  (error: Error                                             ): Incident<string, {}, undefined>;
 
-     <                                C extends Error>(cause: C,                   message : string | ((data: {}) => string)): Incident<"Incident", {}, C        >;
-//   <N extends string,               C extends Error>(cause: C, name: N,                                                   ): Incident<N,          {}, C        >;
-     <N extends string,               C extends Error>(cause: C, name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, C        >;
-                                                      (                            message?: string | ((data: {}) => string)): Incident<"Incident", {}, undefined>;
-//   <N extends string                               >(          name: N,                                                   ): Incident<N,          {}, undefined>;
-     <N extends string                               >(          name: N,          message : string | ((data: {}) => string)): Incident<N,          {}, undefined>;
-     <N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  C        >;
-     <                  D extends {}, C extends Error>(cause: C,          data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  C        >;
-     <N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,          D,  undefined>;
-     <                  D extends {}                 >(                   data: D, message?: string | ((data: D) => string) ): Incident<"Incident", D,  undefined>;
+     <N extends string,               C extends Error>(cause: C, name: N,          message?: string | ((data: {}) => string)): Incident<N,      {}, C        >;
+     <N extends string                               >(          name: N,          message?: string | ((data: {}) => string)): Incident<N,      {}, undefined>;
+     <N extends string, D extends {}, C extends Error>(cause: C, name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,      D,  C        >;
+     <N extends string, D extends {}                 >(          name: N, data: D, message?: string | ((data: D) => string) ): Incident<N,      D,  undefined>;
 }
+// tslint:enable
